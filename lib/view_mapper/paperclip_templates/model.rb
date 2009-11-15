cc17cfef333a2a3c42a61cd113bf60c4d906ff5e
Pat Shaughnessy <pat@patshaughnessy.net>
2009-11-15 12:29:10 -0500

    Old copies of view files removed

--- lib/view_mapper/paperclip_templates/model.rb
+++
@@ -1,8 +0,0 @@
-class <%= class_name %> < ActiveRecord::Base
-<% attributes.select(&:reference?).each do |attribute| -%>
-  belongs_to :<%= attribute.name %>
-<% end -%>
-<% for attachment in attachments -%>
-  has_attached_file :<%= attachment %>
-<% end -%>
-end
